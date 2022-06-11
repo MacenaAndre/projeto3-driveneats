223bf92d2c87e8f51bf47606d9a79c120c0365b5
andremacena <dede.macena94@gmail.com>
2022-06-11 13:41:49 -0300

    Selecionador de produtos

---
+++ codigo.js
@@ -0,0 +1,131 @@
+  function opcao1() {
+    const borda = document.querySelector(".menu-pratos:first-child .pratos:first-child");
+    const icone = document.querySelector(".menu-pratos:first-child .pratos:first-child ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:first-child .pratos:nth-child(2)");
+    const y = document.querySelector(".menu-pratos:first-child .pratos:nth-child(2) ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:first-child .pratos:nth-child(3)");
+    const b = document.querySelector(".menu-pratos:first-child .pratos:nth-child(3) ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+
+  }
+  function opcao2() {
+    const borda = document.querySelector(".menu-pratos:first-child .pratos:nth-child(2)");
+    const icone = document.querySelector(".menu-pratos:first-child .pratos:nth-child(2) ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:first-child .pratos:first-child");
+    const y = document.querySelector(".menu-pratos:first-child .pratos:first-child ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:first-child .pratos:nth-child(3)");
+    const b = document.querySelector(".menu-pratos:first-child .pratos:nth-child(3) ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao3() {
+    const borda = document.querySelector(".menu-pratos:first-child .pratos:nth-child(3)");
+    const icone = document.querySelector(".menu-pratos:first-child .pratos:nth-child(3) ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:first-child .pratos:nth-child(2)");
+    const y = document.querySelector(".menu-pratos:first-child .pratos:nth-child(2) ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:first-child .pratos:first-child");
+    const b = document.querySelector(".menu-pratos:first-child .pratos:first-child ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao4() {
+    const borda = document.querySelector(".menu-pratos:nth-child(2) .pratos:first-child");
+    const icone = document.querySelector(".menu-pratos:nth-child(2) .pratos:first-child ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(2)");
+    const y = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(2) ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(3)");
+    const b = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(3) ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao5() {
+    const borda = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(2)");
+    const icone = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(2) ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:nth-child(2) .pratos:first-child");
+    const y = document.querySelector(".menu-pratos:nth-child(2) .pratos:first-child ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(3)");
+    const b = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(3) ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao6() {
+    const borda = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(3)");
+    const icone = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(3) ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:nth-child(2) .pratos:first-child");
+    const y = document.querySelector(".menu-pratos:nth-child(2) .pratos:first-child ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(2)");
+    const b = document.querySelector(".menu-pratos:nth-child(2) .pratos:nth-child(2) ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao7() {
+    const borda = document.querySelector(".menu-pratos:last-child .pratos:first-child");
+    const icone = document.querySelector(".menu-pratos:last-child .pratos:first-child ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:last-child .pratos:nth-child(2)");
+    const y = document.querySelector(".menu-pratos:last-child .pratos:nth-child(2) ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:last-child .pratos:last-child");
+    const b = document.querySelector(".menu-pratos:last-child .pratos:last-child ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao8() {
+    const borda = document.querySelector(".menu-pratos:last-child .pratos:nth-child(2)");
+    const icone = document.querySelector(".menu-pratos:last-child .pratos:nth-child(2) ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:last-child .pratos:first-child");
+    const y = document.querySelector(".menu-pratos:last-child .pratos:first-child ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:last-child .pratos:last-child");
+    const b = document.querySelector(".menu-pratos:last-child .pratos:last-child ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+  function opcao9() {
+    const borda = document.querySelector(".menu-pratos:last-child .pratos:last-child");
+    const icone = document.querySelector(".menu-pratos:last-child .pratos:last-child ion-icon");
+    borda.classList.toggle("selecionar");
+    icone.classList.toggle("selecionar-icone");
+    const x = document.querySelector(".menu-pratos:last-child .pratos:first-child");
+    const y = document.querySelector(".menu-pratos:last-child .pratos:first-child ion-icon");
+    x.classList.add("selecionar");
+    y.classList.remove("selecionar-icone");
+    const a = document.querySelector(".menu-pratos:last-child .pratos:nth-child(2)");
+    const b = document.querySelector(".menu-pratos:last-child .pratos:nth-child(2) ion-icon");
+    a.classList.add("selecionar");
+    b.classList.remove("selecionar-icone");
+  }
+
+  
+  
+  
